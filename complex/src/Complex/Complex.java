72f109c741989fefad47c7cf109738a1ddb4e7ed
Lich <longzy2013@gmail.com>
2015-04-22 01:23:13 +0800

    interface Modulus

--- complex/src/Complex/Complex.java
+++ complex/src/Complex/Complex.java
@@ -1,3 +1,5 @@
+package Complex;
+
 import java.util.Scanner;
 
 /**
@@ -5,42 +7,59 @@ import java.util.Scanner;
  * 2015-4-8 00:02:46
  * @Author Lich
  */
-public class Complex {
+public class Complex implements ComplexComparable {
     private double real;
     private double imaginary;
     //构造函数
-    public Complex() {
+    public Complex () {
         this.real = 0;
         this.imaginary = 0;
     }
-    public Complex(int real, int imaginary) {
+    public Complex (int real, int imaginary) {
         this.real = real;
         this.imaginary = imaginary;
     }
-    public Complex(float real, float imaginary) {
+    public Complex (float real, float imaginary) {
         this.real = real;
         this.imaginary = imaginary;
     }
-    public Complex(double real, double imaginary) {
+    public Complex (double real, double imaginary) {
         this.real = real;
         this.imaginary = imaginary;
+    }
+
+    public double modulus (Complex complex) {
+        double result;
+        result = Math.sqrt(Math.pow(complex.real, 2) + Math.pow(complex.imaginary, 2));
+        return result;
+    }
+    public static Complex compareModulus(Complex first, Complex second) {
+        Complex result;
+        double f, s;
+        f = first.modulus(first);
+        s = second.modulus(second);
+        if (f > s)
+            result = first;
+        else
+            result = second;
+        return result;
     }
     //加法
-    private static Complex add(Complex first, Complex second) {
+    private static Complex add (Complex first, Complex second) {
         Complex result = new Complex();
         result.real = first.real + second.real;
         result.imaginary = first.imaginary + second.imaginary;
         return result;
     }
     //减法
-    private static Complex take(Complex first, Complex second) {
+    private static Complex take (Complex first, Complex second) {
         Complex result = new Complex();
         result.real = first.real - second.real;
         result.imaginary = first.imaginary - second.imaginary;
         return result;
     }
     //比较
-    private static Complex compare(Complex first, Complex second) {
+    private static Complex compare (Complex first, Complex second) {
         if (first.real > second.real)
             return first;
         else if (first.real == second.real) {
@@ -54,7 +73,7 @@ public class Complex {
         else
             return second;
     }
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         int select = 0;
         double first_real = 0, first_imaginary = 0, second_real = 0, second_imaginary = 0;
         Complex result = new Complex();
@@ -74,7 +93,7 @@ public class Complex {
         Complex first = new Complex(first_real, first_imaginary);
         Complex second = new Complex(second_real, second_imaginary);
         System.out.println("输入要执行的操作:");
-        System.out.println("1.加法; 2.减法; 3.比较大小; 4.描述这两个复数;");
+        System.out.println("1.加法; 2.减法; 3.比较大小; 4.描述这两个复数; 5.比较这两个数模的大小");
         try {
              select = input.nextInt();
         }
@@ -101,6 +120,10 @@ public class Complex {
             case 4:
                 System.out.println("第一个复数实部为"+first.real+", 虚部为"+first.imaginary);
                 System.out.println("第二个复数实部为"+second.real+", 虚部为"+second.imaginary);
+                return;
+            case 5:
+                result = compareModulus(first, second);
+                System.out.println(result.real+"+"+result.imaginary+"i"+"的模较大");
                 return;
             default:
                 System.out.println("未知错误!");
---
+++ complex/src/Complex/ComplexComparable.java
@@ -0,0 +1,8 @@
+package Complex;
+
+/**
+ * Created by lzy on 2015/4/22.
+ */
+public interface ComplexComparable {
+    public double modulus(Complex complex);
+}
